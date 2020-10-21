b4c4f1e6266c6d18078f420749096568b6fecbc0
laisa_crv <laisa0420@gmail.com>
2020-10-21 12:40:15 +0100

    Commit_timer

--- hardware/src/iob_timer.v
+++ hardware/src/iob_timer.v
@@ -28,7 +28,7 @@ module iob_timer (
    	rst_soft_en = 1'b0;
    	tmp_reg_en = 1'b0;
         rdata = 32'b0;
-   	if(valid)
+   	if(ready)
    	  case (address)
    	    `TIMER_RESET:     rst_soft_en = 1'b1;
    	    `TIMER_STOP:      tmp_reg_en = 1'b1;
